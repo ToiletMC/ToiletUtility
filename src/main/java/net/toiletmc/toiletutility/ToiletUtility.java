5bcd373da0cee3b7f230014252812497db7646ee
TheLittle_Yang <2570984321@qq.com>
2025-04-01 16:38:52 +0800

    update

--- src/main/java/net/toiletmc/toiletutility/ToiletUtility.java
+++ src/main/java/net/toiletmc/toiletutility/ToiletUtility.java
@@ -16,6 +16,7 @@ import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
 import io.github.thebusybiscuit.slimefun4.api.SlimefunAddon;
 import io.github.thebusybiscuit.slimefun4.libraries.dough.config.Config;
 import io.github.thebusybiscuit.slimefun4.libraries.dough.items.CustomItemStack;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.UUID;
 
@@ -26,73 +27,47 @@ public class ToiletUtility extends JavaPlugin implements SlimefunAddon {
         // 从 config.yml 中读取插件配置
         Config cfg = new Config(this);
 
-        if (cfg.getBoolean("options.auto-update")) {
-            // 你可以在这里添加自动更新功能
-        }
-
-        /*
-         * 1. 创建分类
-         * 分类的显示物品将使用以下物品
-         */
-        ItemStack itemGroupItem = new CustomItemStack(Material.PLAYER_HEAD, "&x&5&b&7&5&e&e厕所实用工具");
-        String texture = "ewogICJ0aW1lc3RhbXAiIDogMTY4ODM5NzQzNDQwNiwKICAicHJvZmlsZUlkIiA6ICJhZWI3NjNlZDdlNzU0OGQyYWE5N2FmN2E3NjhlMGFlMyIsCiAgInByb2ZpbGVOYW1lIiA6ICJTa3lfQ2FzdGxlXyIsCiAgInNpZ25hdHVyZVJlcXVpcmVkIiA6IHRydWUsCiAgInRleHR1cmVzIiA6IHsKICAgICJTS0lOIiA6IHsKICAgICAgInVybCIgOiAiaHR0cDovL3RleHR1cmVzLm1pbmVjcmFmdC5uZXQvdGV4dHVyZS8xYjNiZTczMzA5OGRkMTZiNTg5MWYxMjE1MGJiMzQ4OGQ0ZTk1MzA0MzYzZDYyZTc3MzAwMGJiMGEzNTA5ZTlmIgogICAgfSwKICAgICJDQVBFIiA6IHsKICAgICAgInVybCIgOiAiaHR0cDovL3RleHR1cmVzLm1pbmVjcmFmdC5uZXQvdGV4dHVyZS8yMzQwYzBlMDNkZDI0YTExYjE1YThiMzNjMmE3ZTllMzJhYmIyMDUxYjI0ODFkMGJhN2RlZmQ2MzVjYTdhOTMzIgogICAgfQogIH0KfQ==";
-
-        // 设置头颅也太麻烦了吧😅
-        SkullMeta skullMeta = (SkullMeta) itemGroupItem.getItemMeta();
-        skullMeta.setPlayerProfile(Bukkit.createProfile(UUID.randomUUID(), null));
-        PlayerProfile playerProfile = skullMeta.getPlayerProfile();
-        playerProfile.getProperties().add(new ProfileProperty("textures", texture));
-        skullMeta.setPlayerProfile(playerProfile);
-        itemGroupItem.setItemMeta(skullMeta);
-
-        // 给你的分类提供一个独一无二的ID
+        // 1. 创建Slimefun分类
         NamespacedKey itemGroupId = new NamespacedKey(this, "toilet_utility");
-        ItemGroup itemGroup = new ItemGroup(itemGroupId, itemGroupItem);
-
-        /*
-         * 2. 创建一个 SlimefunItemStack
-         * 这个类是 ItemStack 的扩展，拥有多个构造函数
-         * 重要：每个物品都得有一个独一无二的ID
-         */
+        ItemGroup itemGroup = new ItemGroup(itemGroupId, getItemGroupIcon());
+        // 2. 创建分类中的物品
         SlimefunItemStack slimefunItem = new SlimefunItemStack("COOL_DIAMOND", Material.DIAMOND, "&4炫酷的钻石", "&c+20% 炫酷");
-
-        /*
-         * 3. 创建配方
-         * 这个配方是一个拥有9个ItemStack的数组。
-         * 它代表了一个3x3的有序合成配方。
-         * 该配方所需的机器将在后面通过RecipeType指定。
-         */
+        // 3. 创建物品的配方
         ItemStack[] recipe = {new ItemStack(Material.EMERALD), null, new ItemStack(Material.EMERALD), null, new ItemStack(Material.DIAMOND), null, new ItemStack(Material.EMERALD), null, new ItemStack(Material.EMERALD)};
-
-        /*
-         * 4. 注册物品
-         * 现在，你只需要注册物品
-         * RecipeType.ENHANCED_CRAFTING_TABLE 代表
-         * 该物品将在增强型工作台中合成。
-         * 来自粘液科技本体的配方类型将会自动将配方添加到对应的机器中。
-         */
+        // 4. 注册物品
         SlimefunItem item = new SlimefunItem(itemGroup, slimefunItem, RecipeType.ENHANCED_CRAFTING_TABLE, recipe);
         item.register(this);
     }
 
     @Override
     public void onDisable() {
-        // 禁用插件的逻辑...
+
     }
 
     @Override
     public String getBugTrackerURL() {
-        // 你可以在这里返回你的问题追踪器的网址，而不是 null
         return "https://github.com/ToiletMC/plugin-ToiletUtility/issues";
     }
 
     @Override
-    public JavaPlugin getJavaPlugin() {
-        /*
-         * 你需要返回对你插件的引用。
-         * 如果这是你插件的主类，只需要返回 "this" 即可。
-         */
+    public @NotNull JavaPlugin getJavaPlugin() {
         return this;
     }
 
+
+    private ItemStack getItemGroupIcon() {
+        ItemStack itemGroupItem = new CustomItemStack(Material.PLAYER_HEAD, "&x&5&b&7&5&e&e厕所实用工具");
+        String texture = "ewogICJ0aW1lc3RhbXAiIDogMTY4ODM5NzQzNDQwNiwKICAicHJvZmlsZUlkIiA6ICJhZWI3NjNlZDdlNzU0OGQyYWE5N2FmN2E3NjhlMGFlMyIsCiAgInByb2ZpbGVOYW1lIiA6ICJTa3lfQ2FzdGxlXyIsCiAgInNpZ25hdHVyZVJlcXVpcmVkIiA6IHRydWUsCiAgInRleHR1cmVzIiA6IHsKICAgICJTS0lOIiA6IHsKICAgICAgInVybCIgOiAiaHR0cDovL3RleHR1cmVzLm1pbmVjcmFmdC5uZXQvdGV4dHVyZS8xYjNiZTczMzA5OGRkMTZiNTg5MWYxMjE1MGJiMzQ4OGQ0ZTk1MzA0MzYzZDYyZTc3MzAwMGJiMGEzNTA5ZTlmIgogICAgfSwKICAgICJDQVBFIiA6IHsKICAgICAgInVybCIgOiAiaHR0cDovL3RleHR1cmVzLm1pbmVjcmFmdC5uZXQvdGV4dHVyZS8yMzQwYzBlMDNkZDI0YTExYjE1YThiMzNjMmE3ZTllMzJhYmIyMDUxYjI0ODFkMGJhN2RlZmQ2MzVjYTdhOTMzIgogICAgfQogIH0KfQ==";
+
+        SkullMeta skullMeta = (SkullMeta) itemGroupItem.getItemMeta();
+        skullMeta.setPlayerProfile(Bukkit.createProfile(UUID.randomUUID(), null));
+        PlayerProfile playerProfile = skullMeta.getPlayerProfile();
+        if (playerProfile != null) {
+            playerProfile.getProperties().add(new ProfileProperty("textures", texture));
+        }
+        skullMeta.setPlayerProfile(playerProfile);
+        itemGroupItem.setItemMeta(skullMeta);
+
+        return itemGroupItem;
+    }
 }
